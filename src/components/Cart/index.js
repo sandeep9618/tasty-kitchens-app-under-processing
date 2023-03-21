88b3a2ccaface538b158429287443e0802260fd3
sandeep9618 <sandeeppappula6@gmail.com>
2023-03-21 07:37:44 +0000

    cart and header details updated

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -8,8 +8,17 @@ class Cart extends Component {
     return (
       <div>
         <Header />
-        <div className="cart-bg-containeer">
-          <img src="" alt="" />
+        <div className="cart-bg-container">
+          <img
+            src="https://res.cloudinary.com/dj3r4fhqp/image/upload/v1679383718/cooking_1_enbhbb.png"
+            alt="empty cart"
+            className="empty-cart-img"
+          />
+          <h1>No Orders Yet!</h1>
+          <p className="empty-cart-description">
+            Your cart is empty. Add something from the menu.
+          </p>
+          <button className="order-now-btn">Order Now</button>
         </div>
       </div>
     )
